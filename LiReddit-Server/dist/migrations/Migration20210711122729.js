b241ee9edb6d42860d1688aa3f4704abea02c852
surya-config <a1b2c3d4config@gmail.com>
2021-07-11 19:55:12 +0530

    first commit

---
+++ LiReddit-Server/dist/migrations/Migration20210711122729.js
@@ -0,0 +1,23 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Migration20210711122729 = void 0;
+const migrations_1 = require("@mikro-orm/migrations");
+class Migration20210711122729 extends migrations_1.Migration {
+    up() {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.addSql('create table "post" ("id" serial primary key, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "title" text not null);');
+            this.addSql('drop table if exists "Post"."Post" cascade;');
+        });
+    }
+}
+exports.Migration20210711122729 = Migration20210711122729;
+//# sourceMappingURL=Migration20210711122729.js.map
